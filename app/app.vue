e096b8ff2bfee4ef7512bd78ebd301538adb382c
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-13 09:10:59 +0000

    feat: add route page transitions

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <template>
   <NuxtLayout>
-    <NuxtPage/>
+    <NuxtPage />
   </NuxtLayout>
-  
+
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s ease;
+}
+.page-enter-from,
+.page-leave-to{
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
