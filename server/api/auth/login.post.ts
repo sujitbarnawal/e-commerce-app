27cf7dee6c7b0f0a946d5dab4074f6661d58f46d
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-25 07:41:52 +0000

    feat(apis): Added signup, login and logout apis for users using database

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,48 +1,64 @@
+import { db } from "~~/server/database";
 import { generateToken, sanitizeUser } from "~~/server/utils/auth";
-import { findUserByEmail } from "~~/server/utils/data";
+import { users } from "../../database/schema";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
 
-export default defineEventHandler(async(event)=>{
-    const body = await readBody(event)
-    const {email,password}=body;
-    if(!email || !password){
-        throw createError({
-            statusCode:400,
-            statusMessage:"Email and password are required"
-        })
-    }
-    const existing=findUserByEmail(email)
-    if(!existing){
-        throw createError({
-            statusCode:404,
-            statusMessage:"User not Found!"
-        })
-    }
-    if(existing.password!==password){
-        throw createError({
-            statusCode:400,
-            statusMessage:"Invalid Password!"
-        })
-    }
-    if(existing.role!=="user"){
-        throw createError(
-            {
-                statusCode:400,
-                statusMessage:"Only users are allowed"
-            }
-        )
-    }
-    const token = generateToken(existing.id)
-    setCookie(event, 'auth_token', token, {
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { email, password } = body;
+  if (!email || !password) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Email and password are required",
+    });
+  }
+  const result = await db.select().from(users).where(eq(users.email, email));
+  const existing = result[0];
+  if (!existing) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "User not Found!",
+    });
+  }
+  const isValid = await bcrypt.compare(password, existing.password);
+  if (!isValid) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Invalid Password!",
+    });
+  }
+  if (existing.role !== "user") {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Only users are allowed",
+    });
+  }
+  const existingResponse = {
+    id: existing.id,
+    name: existing.name,
+    email: existing.email,
+    password: existing.password,
+    phone: existing.phone,
+    role: existing.role,
+    address: {
+      line1: existing.address_line1,
+      line2: existing.address_line2,
+    },
+    createdAt: existing.createdAt,
+  };
+  const token = generateToken(existing.id);
+  setCookie(event, "auth_token", token, {
     httpOnly: true,
-    sameSite: 'lax',
-    path: '/',
+    sameSite: "lax",
+    path: "/",
     maxAge: 60 * 60 * 24,
-  })
-    return {
-        success:true,
-        data:{
-            token,
-            user:sanitizeUser(existing)
-        }
-    }
-})
+  });
+  return {
+    success: true,
+    data: {
+      token,
+      user: sanitizeUser(existingResponse),
+    },
+  };
+});
--- server/api/auth/signup.post.ts
+++ server/api/auth/signup.post.ts
@@ -1,5 +1,8 @@
+import { db } from "~~/server/database";
 import { generateToken, sanitizeUser } from "~~/server/utils/auth";
-import { createUser, findUserByEmail } from "~~/server/utils/data";
+import bcrypt from "bcrypt"
+import {users} from "../../database/schema"
+import { eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
@@ -11,23 +14,34 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const existingUser = findUserByEmail(email);
+  const result = await db.select().from(users).where(eq(users.email,email)).limit(1)
+  const existingUser = result[0]
   if (existingUser) {
     throw createError({
       statusCode: 409,
       statusMessage: "User already exists!",
     });
   }
-  const newUser = createUser({
-    email,
-    password,
-    name,
-    address: {
-      line1: null,
-      line2: null,
+  const hashedPassword = await bcrypt.hash(password,10)
+  const newUserResult = await db.insert(users).values({
+    name:name,
+    email:email,
+    password:hashedPassword
+  }).returning()
+  const newUser = newUserResult[0]
+  const newUserResponse={
+    id:newUser.id,
+    name:newUser.name,
+    email:newUser.email,
+    password:newUser.password,
+    phone:newUser.phone,
+    role:newUser.role,
+    address:{
+      line1:newUser.address_line1,
+      line2:newUser.address_line2
     },
-    phone: null,
-  });
+    createdAt:newUser.createdAt
+  }
   const token = generateToken(newUser.id);
    setCookie(event, 'auth_token', token, {
     httpOnly: true,
@@ -38,7 +52,7 @@ export default defineEventHandler(async (event) => {
   return {
     success: true,
     data: {
-      user: sanitizeUser(newUser),
+      user: sanitizeUser(newUserResponse),
       token,
     },
   };
