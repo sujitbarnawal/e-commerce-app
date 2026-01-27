978d397b9ec768035b5165e3d55208f344a2870f
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-27 05:20:24 +0000

    fix: Added run time config instead of dotenv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css:['/assets/css/main.css'],
+  css: ["/assets/css/main.css"],
 
-  app:{
-    head:{
-      titleTemplate:'%s - Online Pasal',
-      meta:[
-        {name:"description",content:"Online Pasal"}
-      ]
+  app: {
+    head: {
+      titleTemplate: "%s - Online Pasal",
+      meta: [{ name: "description", content: "Online Pasal" }],
     },
-    pageTransition:{
-      name:"page",
-      mode:"out-in"
-    }
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
   },
 
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+  },
+});
